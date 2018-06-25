51f75e54374e246dbe7a4d0843d3546cc39574c2
mbarnett51 <mbarnett18@amherst.edu>
2018-06-25 12:12:23 -0400

    Add list books, and console now has an example library

--- src/com/twu/biblioteca/Console.java
+++ src/com/twu/biblioteca/Console.java
@@ -1,21 +1,50 @@
 package com.twu.biblioteca;
 
+import java.util.*;
+
 class Console {
 
-    Console() {
+    private ArrayList<String> library;
 
+    Console() {
+        SetUpLibrary();
+    }
+
+    private void SetUpLibrary() {
+        library = new ArrayList<String>();
+        library.add("The Color Purple");
+        library.add("Emma");
+        library.add("Schroder");
+        library.add("True Grit");
+        library.add("Beloved");
+        library.add("Kiss of the Spider Woman");
+        library.add("Lord of the Flies");
+        library.add("White Tiger");
     }
 
     String getWelcomeMessage() {
-        return "Hello! \n Welcome to Biblioteca. \n Please type your commands below.";
+        return "Hello!\nWelcome to Biblioteca.\nPlease type your commands below.";
     }
 
     String getMainMenu() {
-        return "MAIN MENU: \n List Books";
+        return "MAIN MENU:\nList Books";
     }
 
     String readMessage(String in) {
-        return "Invalid menu option.  Try again.";
+        if (in.equals("List Books")) {
+            return listBooks();
+        } else {
+            return "Invalid menu option.  Try again.";
+        }
+    }
+
+    private String listBooks() {
+        StringBuilder listOfBooks = new StringBuilder(library.get(0));
+        for (int i = 1; i < library.size(); i++) {
+            String nextBook = "\n" + library.get(i);
+            listOfBooks.append(nextBook);
+        }
+        return listOfBooks.toString();
     }
 
 }
--- test/com/twu/biblioteca/ConsoleTest.java
+++ test/com/twu/biblioteca/ConsoleTest.java
@@ -8,21 +8,29 @@ public class ConsoleTest {
     @Test
     public void testWelcomeMessage() {
         Console console = new Console();
-        assertEquals(console.getWelcomeMessage(), "Hello! \n Welcome to Biblioteca. " +
-                "\n Please type your commands below.");
+        assertEquals(console.getWelcomeMessage(), "Hello!\nWelcome to Biblioteca." +
+                "\nPlease type your commands below.");
     }
 
     @Test
     public void testMainMenu() {
         Console console = new Console();
-        assertEquals(console.getMainMenu(), "MAIN MENU: \n List Books");
+        assertEquals(console.getMainMenu(), "MAIN MENU:\nList Books");
     }
 
     @Test
-    public void testReadInvalidInput() {
+    public void testReadMessageWithInvalidInput() {
         Console console = new Console();
         String message = "This is invalid";
         assertEquals(console.readMessage(message), "Invalid menu option.  Try again.");
+    }
+
+    @Test
+    public void testReadMessageWithListBooks() {
+        Console console = new Console();
+        String message = "List Books";
+        assertEquals(console.readMessage(message), "The Color Purple\nEmma\nSchroder\nTrue Grit" +
+                "\nBeloved\nKiss of the Spider Woman\nLord of the Flies\nWhite Tiger");
     }
 }
 
